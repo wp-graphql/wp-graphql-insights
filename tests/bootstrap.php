99e1f5c32ab5c1665e4ed0684102801b678dc7a1
Jason <jasonbahl@mac.com>
2017-09-26 08:22:05 -0600

    Adding Unit Tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,7 +7,7 @@
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
+	$_tests_dir = '/tmp/wp-graphql-insights/wordpress-tests-lib';
 }
 
 // Give access to tests_add_filter() function.
