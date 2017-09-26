36d1a6d595bffe86944423cd2c88dfe90ef827ee
Jason <jasonbahl@mac.com>
2017-09-26 12:14:13 -0600

    Adding Unit Tests

--- tests/test-WPGraphQL.php
+++ tests/test-WPGraphQL.php
@@ -1,12 +1,15 @@
 <?php
 
-class Test_WPGraphQL extends WP_UnitTestCase {
+class Test_WPGraphQL_Integration extends WP_UnitTestCase {
 
-	function testGraphQLQuery() {
+	/**
+	 * Let's ensure that Tracing is output as expected in the extensions of the GraphQL Response
+	 */
+	function testGraphQLQueryTracingInResponse() {
 		$query = '{posts{edges{node{id}}}}';
 		$results = do_graphql_request( $query );
-		var_dump( $results );
 		$this->assertArrayNotHasKey( 'errors', $results );
+		$this->assertArrayHasKey( 'tracing', $results['extensions'] );
 	}
 
 }
